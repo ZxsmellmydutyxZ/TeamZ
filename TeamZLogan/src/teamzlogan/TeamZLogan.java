035a3c6a91a1d09a468bb8ed9326a67dbf8fea71
lcoon1048 <lcoon1048@AHHDB11C6BD176.inst.hcpss.org>
2015-11-18 09:42:13 -0500

    Test Logan 1

---
+++ TeamZLogan/src/teamzlogan/TeamZLogan.java
@@ -0,0 +1,17 @@
+
+package teamzlogan;
+
+/**
+ *
+ * @author lcoon1048
+ */
+public class TeamZLogan {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
